48a09af12bc998ea4be684b87dd4e1af9e9fff52
andre <andrew260602sush@gmail.com>
2023-09-18 22:58:01 +0300

    Добавил панельку и стейты

--- Praph2/Form1.Designer.cs
+++ Praph2/Form1.Designer.cs
@@ -29,19 +29,20 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            trackBar1 = new TrackBar();
-            trackBar2 = new TrackBar();
-            label1 = new Label();
-            label2 = new Label();
-            trackBar3 = new TrackBar();
             label3 = new Label();
+            trackBar3 = new TrackBar();
+            label2 = new Label();
+            label1 = new Label();
+            trackBar2 = new TrackBar();
+            trackBar1 = new TrackBar();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel1 = new Panel();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
@@ -68,46 +69,30 @@
             groupBox1.Text = "Панелька";
             groupBox1.Enter += groupBox1_Enter;
             // 
-            // button1
+            // label3
             // 
-            button1.Location = new Point(36, 72);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Задание 1";
-            button1.UseVisualStyleBackColor = true;
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 375);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Яркость";
             // 
-            // button2
+            // trackBar3
             // 
-            button2.Location = new Point(36, 153);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Задание 2";
-            button2.UseVisualStyleBackColor = true;
+            trackBar3.Location = new Point(20, 393);
+            trackBar3.Name = "trackBar3";
+            trackBar3.Size = new Size(124, 45);
+            trackBar3.TabIndex = 7;
             // 
-            // button3
+            // label2
             // 
-            button3.Location = new Point(36, 221);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 2;
-            button3.Text = "Задание 3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // trackBar1
-            // 
-            trackBar1.Location = new Point(20, 266);
-            trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(124, 45);
-            trackBar1.TabIndex = 3;
-            // 
-            // trackBar2
-            // 
-            trackBar2.Location = new Point(20, 332);
-            trackBar2.Name = "trackBar2";
-            trackBar2.Size = new Size(124, 45);
-            trackBar2.TabIndex = 4;
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 314);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Насыщенность";
             // 
             // label1
             // 
@@ -119,44 +104,73 @@
             label1.Text = "Оттенок";
             label1.Click += label1_Click;
             // 
-            // label2
+            // trackBar2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(20, 314);
-            label2.Name = "label2";
-            label2.Size = new Size(92, 15);
-            label2.TabIndex = 6;
-            label2.Text = "Насыщенность";
+            trackBar2.Location = new Point(20, 332);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(124, 45);
+            trackBar2.TabIndex = 4;
             // 
-            // trackBar3
+            // trackBar1
             // 
-            trackBar3.Location = new Point(20, 393);
-            trackBar3.Name = "trackBar3";
-            trackBar3.Size = new Size(124, 45);
-            trackBar3.TabIndex = 7;
+            trackBar1.Location = new Point(20, 266);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(124, 45);
+            trackBar1.TabIndex = 3;
             // 
-            // label3
+            // button3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(20, 375);
-            label3.Name = "label3";
-            label3.Size = new Size(51, 15);
-            label3.TabIndex = 8;
-            label3.Text = "Яркость";
+            button3.Location = new Point(36, 221);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Задание 3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(36, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Задание 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(36, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Задание 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(650, 450);
+            panel1.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -173,5 +187,6 @@
         private Button button1;
         private Label label3;
         private TrackBar trackBar3;
+        private Panel panel1;
     }
 }
--- Praph2/Form1.cs
+++ Praph2/Form1.cs
@@ -2,6 +2,8 @@ namespace Praph2
 {
     public partial class Form1 : Form
     {
+        State curentState = State.Task0;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +11,33 @@ namespace Praph2
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            curentState = State.Task3;
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            curentState = State.Task2;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            curentState = State.Task1;
+
+        }
+    }
+
+    enum State
+    {
+        Task0, Task1, Task2, Task3
     }
 }
