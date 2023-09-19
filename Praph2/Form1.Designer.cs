728b6183c6adb2c6832bbf7433221306f05cc3ea
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2023-09-19 14:32:22 +0300

    Added basic functionality&files

--- Praph2/Form1.Designer.cs
+++ Praph2/Form1.Designer.cs
@@ -38,7 +38,7 @@
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
-            panel1 = new Panel();
+            Canvas = new Panel();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)trackBar3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
@@ -60,10 +60,12 @@
             groupBox1.Controls.Add(button2);
             groupBox1.Controls.Add(button1);
             groupBox1.Dock = DockStyle.Right;
-            groupBox1.Location = new Point(650, 0);
-            groupBox1.MinimumSize = new Size(150, 0);
+            groupBox1.Location = new Point(743, 0);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.MinimumSize = new Size(171, 0);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(150, 450);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(171, 600);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Панелька";
@@ -72,57 +74,61 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(20, 375);
+            label3.Location = new Point(23, 500);
             label3.Name = "label3";
-            label3.Size = new Size(51, 15);
+            label3.Size = new Size(64, 20);
             label3.TabIndex = 8;
             label3.Text = "Яркость";
             // 
             // trackBar3
             // 
-            trackBar3.Location = new Point(20, 393);
+            trackBar3.Location = new Point(17, 524);
+            trackBar3.Margin = new Padding(3, 4, 3, 4);
             trackBar3.Name = "trackBar3";
-            trackBar3.Size = new Size(124, 45);
+            trackBar3.Size = new Size(142, 56);
             trackBar3.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(20, 314);
+            label2.Location = new Point(23, 419);
             label2.Name = "label2";
-            label2.Size = new Size(92, 15);
+            label2.Size = new Size(114, 20);
             label2.TabIndex = 6;
             label2.Text = "Насыщенность";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(20, 246);
+            label1.Location = new Point(23, 328);
             label1.Name = "label1";
-            label1.Size = new Size(52, 15);
+            label1.Size = new Size(65, 20);
             label1.TabIndex = 5;
             label1.Text = "Оттенок";
             label1.Click += label1_Click;
             // 
             // trackBar2
             // 
-            trackBar2.Location = new Point(20, 332);
+            trackBar2.Location = new Point(17, 443);
+            trackBar2.Margin = new Padding(3, 4, 3, 4);
             trackBar2.Name = "trackBar2";
-            trackBar2.Size = new Size(124, 45);
+            trackBar2.Size = new Size(142, 56);
             trackBar2.TabIndex = 4;
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(20, 266);
+            trackBar1.Location = new Point(17, 355);
+            trackBar1.Margin = new Padding(3, 4, 3, 4);
             trackBar1.Name = "trackBar1";
-            trackBar1.Size = new Size(124, 45);
+            trackBar1.Size = new Size(142, 56);
             trackBar1.TabIndex = 3;
             // 
             // button3
             // 
-            button3.Location = new Point(36, 221);
+            button3.Location = new Point(47, 268);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(90, 30);
             button3.TabIndex = 2;
             button3.Text = "Задание 3";
             button3.UseVisualStyleBackColor = true;
@@ -130,9 +136,10 @@
             // 
             // button2
             // 
-            button2.Location = new Point(36, 153);
+            button2.Location = new Point(47, 167);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(90, 30);
             button2.TabIndex = 1;
             button2.Text = "Задание 2";
             button2.UseVisualStyleBackColor = true;
@@ -140,32 +147,35 @@
             // 
             // button1
             // 
-            button1.Location = new Point(36, 72);
+            button1.Location = new Point(47, 69);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(90, 30);
             button1.TabIndex = 0;
             button1.Text = "Задание 1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // panel1
+            // Canvas
             // 
-            panel1.BackColor = SystemColors.ActiveBorder;
-            panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(650, 450);
-            panel1.TabIndex = 1;
+            Canvas.BackColor = SystemColors.Window;
+            Canvas.Dock = DockStyle.Fill;
+            Canvas.Location = new Point(0, 0);
+            Canvas.Margin = new Padding(3, 4, 3, 4);
+            Canvas.Name = "Canvas";
+            Canvas.Size = new Size(743, 600);
+            Canvas.TabIndex = 1;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel1);
+            ClientSize = new Size(914, 600);
+            Controls.Add(Canvas);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Графика. Лабораторная 2";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)trackBar3).EndInit();
@@ -187,6 +197,6 @@
         private Button button1;
         private Label label3;
         private TrackBar trackBar3;
-        private Panel panel1;
+        private Panel Canvas;
     }
 }
--- Praph2/Form1.cs
+++ Praph2/Form1.cs
@@ -1,12 +1,17 @@
+using FastBitmap;
+
 namespace Praph2
 {
     public partial class Form1 : Form
     {
         State curentState = State.Task0;
 
+        private Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+            graphics = Canvas.CreateGraphics();
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -17,21 +22,29 @@ namespace Praph2
         {
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        //Преобразовать изображение из RGB в оттенки серого. Реализовать два варианта формулы с учетом разных вкладов R, G и B в интенсивность (см презентацию).
+        //Затем найти разность полученных полутоновых изображений. Построить гистограммы интенсивности после одного и второго преобразования.
+        private void button1_Click(object sender, EventArgs e)
         {
-            curentState = State.Task3;
-
+            curentState = State.Task1;
         }
 
+        //Выделить из полноцветного изображения каждый из каналов R, G, B  и вывести результат. Построить гистограмму по цветам (3 штуки).
         private void button2_Click(object sender, EventArgs e)
         {
             curentState = State.Task2;
 
+            graphics.Clear(Color.White);
+
+            Bitmap bitmap =  new Bitmap("../../../../images/ФРУКТЫ.jpg");
+            graphics.DrawImage(bitmap, 0, 0);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //Преобразовать изображение из RGB в HSV. Добавить возможность изменять значения оттенка, насыщенности и яркости.
+        //Результат сохранять в файл, предварительно преобразовав обратно.
+        private void button3_Click(object sender, EventArgs e)
         {
-            curentState = State.Task1;
+            curentState = State.Task3;
 
         }
     }
